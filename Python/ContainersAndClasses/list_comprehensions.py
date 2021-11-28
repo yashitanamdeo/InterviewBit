4ebbf0ea8000b0ec37b63d30394012ed3db4f046
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-28 23:07:23 +0530

    update List Comprehensions

--- Python/ContainersAndClasses/list_comprehensions.py
+++ Python/ContainersAndClasses/list_comprehensions.py
@@ -2,6 +2,8 @@
 
 def main():
     str_list = ['given', 'intern', 'InterviewBit', 'network', 'local', 'multiple', 'define', 'nodes', 'algorithm', 'allows', 'community', 'phase', 'single']
+    # List comprehension
+    #my_list = [x for x in str_list if len(x) %2 == 1]
     my_list = []
     for i in str_list:
         if len(i) % 2 != 0:
