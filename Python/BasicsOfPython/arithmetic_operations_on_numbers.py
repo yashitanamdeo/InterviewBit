cfa7caf8e486ffd1af40b18577322fff935c5834
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-28 15:15:57 +0530

    add Arithmetic operators on numbers

--- Python/BasicsOfPython/arithmetic_operations_on_numbers.py
+++ Python/BasicsOfPython/arithmetic_operations_on_numbers.py
@@ -1,1 +1,17 @@
 '''Problem Statement:https://www.interviewbit.com/problems/arithmetic-operators-on-numbers/'''
+
+def main():
+    A = int(input())
+    B = int(input())
+    # Print seven lines as described above
+    print(A+B)
+    print(A-B)
+    print(A*B)
+    print(A//B)
+    print(A/B)
+    print(A%B)
+    print(A**B)
+    return 0
+
+if __name__ == '__main__':
+    main()
