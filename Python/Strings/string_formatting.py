3e5144897051f5345d3102dbce835662e63023d4
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-28 16:54:43 +0530

    update String formatting

--- Python/Strings/string_formatting.py
+++ Python/Strings/string_formatting.py
@@ -4,6 +4,7 @@ def main():
     data = ("Robin", 10, "chocolates")
     format_string = None
     print("Hello {0}. You are currently left with {1} {2}.".format(data[0],data[1],data[2]))
+    #print(f"Hello{data[0]}. You are currently left with {data[1]} {data[2]}.)  --> fstring
     return 0
 
 if __name__ == '__main__':
