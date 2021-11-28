c6039de2a7141bc0a8cca1ad7cb2b543a3f602ab
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-28 23:04:59 +0530

    add List Comprehensions

--- Python/ContainersAndClasses/list_comprehensions.py
+++ Python/ContainersAndClasses/list_comprehensions.py
@@ -1,1 +1,15 @@
 '''Problem Statement:https://www.interviewbit.com/problems/list-comprehensions/'''
+
+def main():
+    str_list = ['given', 'intern', 'InterviewBit', 'network', 'local', 'multiple', 'define', 'nodes', 'algorithm', 'allows', 'community', 'phase', 'single']
+    my_list = []
+    for i in str_list:
+        if len(i) % 2 != 0:
+            my_list.append(i)
+        else:
+            continue
+    print(my_list)
+    return 0
+
+if __name__ == '__main__':
+    main()
