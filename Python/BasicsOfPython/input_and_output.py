7836c993d0209d7b99ea94b720884a609b727a87
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-28 12:30:33 +0530

    add input and output

---
+++ Python/BasicsOfPython/input_and_output.py
@@ -0,0 +1,11 @@
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    string = input()
+    print("Hello " + string)
+    return 0 
+
+
+if __name__ == '__main__':
+    main()
