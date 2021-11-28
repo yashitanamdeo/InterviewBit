9caadd06b3dbe15d41c921151b9fc8a705c9be0e
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-29 01:16:34 +0530

    add Combinatoric Iterators

--- Python/Itertools&Collections/combinatoric_iterators.py
+++ Python/Itertools&Collections/combinatoric_iterators.py
@@ -1,1 +1,15 @@
-'''Problem Startement:https://www.interviewbit.com/problems/itertools-combinatoric-iterators/'''
+'''Problem Statement:https://www.interviewbit.com/problems/itertools-combinatoric-iterators/'''
+
+from itertools import permutations
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    S, K = input().split()
+
+    for i in permutations(sorted(S),int(K)):
+        print("".join(i))
+    return 0
+
+if __name__ == '__main__':
+    main()
