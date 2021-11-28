cd5882c3193e9cc826cac1a21ab7f0fb46330baf
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-28 16:59:11 +0530

    add String operations

--- Python/Strings/string_operations.py
+++ Python/Strings/string_operations.py
@@ -1,1 +1,18 @@
 '''Problem Statement:https://www.interviewbit.com/problems/string-operations/'''
+
+def main():
+    S = input()
+    
+    #Print length of the string S
+    print(len(S))
+    
+    #Print the first occurence of the letter 'a' in S
+    #Note it is guaranteed that letter a is present in the string S
+    print(S.index("a"))
+
+    #Print all the characters with even index in S
+    print(S[::2])
+    return 0
+
+if __name__ == '__main__':
+    main()
