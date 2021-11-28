31935d586cb2789370af347f2498ec05e06f09d9
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-29 01:44:30 +0530

    add Collections module tutorial

--- Python/Itertools&Collections/collections_module_tutorial.py
+++ Python/Itertools&Collections/collections_module_tutorial.py
@@ -1,1 +1,23 @@
 '''Problem Statement:https://www.interviewbit.com/problems/collections-module-tutorial/'''
+
+from collections import defaultdict
+def main():
+    A = input().split()
+    N = len(A)
+    B = input().split()
+    M = len(B)
+    # Your code goes here
+    d = defaultdict(list)
+    for i in range(N):
+        d[A[i]].append(i)
+    for i in B:
+        if(len(d[i]) == 0):
+            print(-1)
+        else:
+            for j in d[i]:
+                print (j, end = ' ')
+            print()
+    return 0
+
+if __name__ == '__main__':
+    main()
