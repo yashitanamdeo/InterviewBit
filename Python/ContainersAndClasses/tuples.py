a7b0055d78efb5330e238079ca4be75cff82ce9f
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-28 17:26:29 +0530

    add Tuples

--- Python/ContainersAndClasses/tuples.py
+++ Python/ContainersAndClasses/tuples.py
@@ -1,1 +1,24 @@
 '''Problem Statement:https://www.interviewbit.com/problems/tuples/'''
+
+def main():
+    tuple1 = tuple(("one", "two", "three"))
+    tuple2 = tuple(("1", "2", "3"))
+    
+    # change value at index 0 of both tuple to string "number"
+    # Your code goes here
+    list1 = list(tuple1)
+    list2 = list(tuple2)
+
+    list1[0] = 'number'
+    list2[0] = 'number'
+
+    tuple1 = tuple(list1)
+    tuple2 = tuple(list2)
+
+    print(tuple1)
+    print(tuple2)
+    
+    return 0
+
+if __name__ == '__main__':
+    main()
