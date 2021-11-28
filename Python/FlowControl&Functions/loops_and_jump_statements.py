c3ee0bf97af89452db20a5cf60f6ac2c265bea0b
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-28 16:42:16 +0530

    add Loops and Jump statements

--- Python/FlowControl&Functions/loops_and_jump_statements.py
+++ Python/FlowControl&Functions/loops_and_jump_statements.py
@@ -1,1 +1,16 @@
 '''Problem Statement:https://www.interviewbit.com/problems/loops-and-jump-statements/'''
+
+def main():
+    N = int(input())
+    # YOUR CODE GOES HERE
+    for i in range(N):
+        if i%2!=0:
+            print(i)
+        else:
+            continue
+        
+
+    return 0
+
+if __name__ == '__main__':
+    main()
