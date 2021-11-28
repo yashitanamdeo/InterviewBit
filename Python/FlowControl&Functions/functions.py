48d13c83011fe749d1fd7354406929b4aab1a338
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-28 16:43:43 +0530

    add Functions

--- Python/FlowControl&Functions/functions.py
+++ Python/FlowControl&Functions/functions.py
@@ -1,1 +1,9 @@
 '''Problem Statement:https://www.interviewbit.com/problems/functions/'''
+
+#Return the factorial of the number N
+def factorial(N):
+    # Your code goes here
+    fact = 1
+    for i in range(1,N+1):
+        fact*=i
+    return fact
