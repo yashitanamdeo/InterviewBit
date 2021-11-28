135c183eea20e767a356913d8b2e878796d3a295
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-29 01:49:39 +0530

    add Collections Module I

--- Python/Itertools&Collections/collections_module_I.py
+++ Python/Itertools&Collections/collections_module_I.py
@@ -1,1 +1,19 @@
 '''Problem Statement:https://www.interviewbit.com/problems/collections-module-i/'''
+
+from collections import Counter
+
+def main():
+    N = int(input())
+    d = Counter(map(int, input().split()))
+    M = int(input())
+    ans = 0
+    for i in range(M):
+        xi, typ = map(int, input().split())
+        if typ in d and d[typ] > 0:
+            ans += xi
+            d[typ] -= 1
+    print(ans)    
+    return 0
+
+if __name__ == '__main__':
+    main()
