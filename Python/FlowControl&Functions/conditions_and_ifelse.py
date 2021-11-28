d087c2b1ce6fb307ec8c048179728512d4c3dde3
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-28 16:16:50 +0530

    add Conditions and Ifelse

--- Python/FlowControl&Functions/conditions_and_ifelse.py
+++ Python/FlowControl&Functions/conditions_and_ifelse.py
@@ -1,1 +1,23 @@
 '''Problem Statement:https://www.interviewbit.com/problems/conditions-and-if-else/'''
+
+def main():
+    num = int(input())
+    # YOUR CODE GOES HERE
+    if num>=90:
+        print('A')
+    elif num>=80:
+        print('B')
+    elif num>=70:
+        print('C')
+    elif num>=60:
+        print('D')
+    if num>=50:
+        print('E')
+    else:
+        print('F')
+
+
+    return 0
+
+if __name__ == '__main__':
+    main()
