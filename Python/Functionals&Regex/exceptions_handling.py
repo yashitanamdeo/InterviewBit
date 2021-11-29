649be098782806ae8600c3bbd75e0f5fc7f12028
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-29 22:48:37 +0530

    add Exceptions Handling

--- Python/Functionals&Regex/exceptions_handling.py
+++ Python/Functionals&Regex/exceptions_handling.py
@@ -1,1 +1,15 @@
 '''Problem Statement:https://www.interviewbit.com/problems/exceptions-handling/'''
+
+def main():
+    for i in range(int(input())):
+        try:
+            a,b = map(int, input().split())
+            print (a//b)
+        except ZeroDivisionError as e:
+            print ('Error Code:',e)
+        except ValueError as e:
+            print ('Error Code:',e)
+    return 0
+
+if __name__ == '__main__':
+    main()
