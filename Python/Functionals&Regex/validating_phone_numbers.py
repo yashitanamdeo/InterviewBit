e40caf4f92138d42c81926e968a9ab7f1e1aace4
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-29 22:34:34 +0530

    add Validating Phone Numbers

--- Python/Functionals&Regex/validating_phone_numbers.py
+++ Python/Functionals&Regex/validating_phone_numbers.py
@@ -1,1 +1,13 @@
 '''Problem Statement:https://www.interviewbit.com/problems/validating-phone-numbers/'''
+
+import re
+def main():
+    for i in range(int(input())):
+        if re.search(r"^[6789]\d{9}$",input()):
+            print('YES') 
+        else:
+            print('NO')
+    return 0
+
+if __name__ == '__main__':
+    main()
